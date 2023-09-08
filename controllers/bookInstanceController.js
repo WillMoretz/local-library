748f91cf86d95853d52acb158a2f6a661bc6f6d7
Will Moretz <whmoretz@gmail.com>
2023-09-08 13:01:36 -0400

    added delete functions

--- controllers/bookInstanceController.js
+++ controllers/bookInstanceController.js
@@ -92,12 +92,24 @@ exports.bookinstance_create_post = [
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete GET");
+  // Get Book Instance details
+  const bookinstance = await BookInstance.findById(req.params.id).exec();
+
+  if (bookinstance === null) {
+    // No Result
+    res.redirect("/catalog/bookinstances");
+  }
+
+  res.render("bookinstance_delete", {
+    title: "Book Instance Delete",
+    bookinstance: bookinstance,
+  });
 });
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete POST");
+  await BookInstance.findByIdAndRemove(req.body.bookinstanceid);
+  res.redirect("/catalog/bookinstances");
 });
 
 // Display BookInstance update form on GET.
