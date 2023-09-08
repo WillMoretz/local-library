83f1c46a9ac228e74cb359a74ac64686f569d8bd
Will Moretz <whmoretz@gmail.com>
2023-09-08 10:03:07 -0400

    added another formatted date virtual

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -17,13 +17,16 @@ const BookInstanceSchema = new Schema({
 
 // Virtual for bookinstance's URL
 BookInstanceSchema.virtual("url").get(function () {
-  // We don't use an arrow function as we'll need the this object
   return `/catalog/bookinstance/${this._id}`;
 });
 
 // Virtual for formatted due back date
 BookInstanceSchema.virtual("due_back_formatted").get(function () {
   return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+});
+
+BookInstanceSchema.virtual("due_back_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.due_back).toISODate(); // format 'YYYY-MM-DD'
 });
 
 // Export model
